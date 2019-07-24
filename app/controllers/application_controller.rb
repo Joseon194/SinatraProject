2bb4810689fb133a8bf6c1d24ca11f20854ce828
Dennis Chung <chungdennis6@gmail.com>
2019-07-24 16:58:43 +0000

    created sessions_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,7 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "sinatra_project"
   end
 
-    get '/' do
-      session[:greeting] = "Hello World"
-      cookies[:a_dangerous_value] = "You have $100 in credit!"
-      "Hello World!"
-    end
 
-    get '/remember' do
-      session[:greeting]
 end
 
 end
