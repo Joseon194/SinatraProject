73ddb7e68cacf631e7bfe132b4626c66dad4bbce
Dennis Chung <chungdennis6@gmail.com>
2019-07-24 16:12:09 +0000

    edited environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => db/development.sqlite
+  :database => "db/development.sqlite"
 )
 require_all 'app'
